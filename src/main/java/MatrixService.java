3e3167a4310d974ffd0e8fe23f9d0777421b0ecb
Nikita Asaevich <nasaevich459@gmail.com>
2023-05-15 09:04:40 +0300

    Initial commit

---
+++ src/main/java/MatrixService.java
@@ -0,0 +1,175 @@
+public class MatrixService
+{
+
+    public static void printMatrix(double[][] matrix)
+    {
+        System.out.println( " " );
+        System.out.println( " a:" );
+        for(int i = 0 ; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                System.out.print( matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static double matrixCell(double[][] firstMatrix, double[][] secondMatrix, int row, int column)
+    {
+        double cell = 0;
+        for (int i = 0; i < secondMatrix.length; i++) {
+            cell += firstMatrix[row][i] * secondMatrix[i][column];
+        }
+        return cell;
+    }
+
+    public static double[][] sumMatrix(double[][] firstMatrix, double[][] secondMatrix){
+        int i, j;
+        double[][] retMatrix = new double[firstMatrix.length][secondMatrix.length];
+
+        for (i = 0; i < firstMatrix.length; i++)
+            for (j = 0; j < firstMatrix.length; j++) {
+                retMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+
+        return retMatrix;
+    }
+
+    public static double[][] divMatrix(double[][] firstMatrix, double[][] secondMatrix){
+        int i, j;
+        double[][] retMatrix = new double[firstMatrix.length][secondMatrix.length];
+
+        for (i = 0; i < firstMatrix.length; i++)
+            for (j = 0; j < firstMatrix.length; j++) {
+                retMatrix[i][j] = firstMatrix[i][j] - secondMatrix[i][j];
+            }
+
+        return retMatrix;
+    }
+    public static double[][] subtractionMatrix(double[][] firstMatrix, double[][] secondMatrix){
+        int i, j;
+        double[][] retMatrix = new double[firstMatrix.length][secondMatrix.length];
+
+        for (i = 0; i < firstMatrix.length; i++)
+            for (j = 0; j < firstMatrix.length; j++) {
+                retMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+
+        return retMatrix;
+    }
+
+    public static double matr_inf_norm ( double[][] a){
+        int i,j;
+        double s,norm = 0.;
+
+        for( i = 0; i < a.length; i++ )
+        {
+            for( s = 0., j = 0; j < a.length; j++ ) s += Math.abs( a[i][j] );
+            if( Double.compare(s, norm) > 0 ) norm = s;
+        }
+
+        return norm;
+    }
+
+    public static int UpTriangleMatrix(double[][] arr)
+    {
+        int size = arr.length;
+        //Привидение к треугольному виду
+        double mod;
+        double cos, sin;
+        double t;
+
+
+        for(int k = 0; k < size-1; k++)
+        {
+            for(int i = k+1; i < size; i++)
+            {
+                mod = Math.sqrt(arr[k][k] * arr[k][k] + arr[i][k] * arr[i][k]);
+                if(mod != 0) {
+                    sin = -arr[i][k] / mod;
+                    cos = arr[k][k] / mod;
+                    System.out.println(sin);
+                    System.out.println(cos);
+                } else
+                {
+                    return 0;
+                }
+                for(int j = k; j < size; j++)
+                {
+                    t = arr[k][j];
+                    arr[k][j] = cos*t - sin * arr[i][j];
+                    arr[i][j] = sin*t + cos*arr[i][j];
+
+                }
+                if(Double.compare(sin, 0) > 0) {
+                    arr[i][k] = cos+1.;
+                }else {
+                    arr[i][k] = cos-1.;
+                }
+
+            }
+        }
+        printMatrix(arr);
+        return 1;
+    }
+
+    public static int reverseMatrix(double[][] upTriangle)
+    {
+        double sum = 0.;
+        double[] arr = new double[upTriangle.length];
+        for (int i = 0; i < upTriangle.length; i++)
+        {
+            upTriangle[i][i] = 1./upTriangle[i][i];
+        }
+
+        for (int i = arr.length-1; i >= 0; i--)
+        {
+            for(int j = i+1; j<arr.length; j++) {
+                arr[j] = upTriangle[i][j];
+            }
+            for(int j = i+1; j<arr.length; j++)
+            {
+                sum =0.;
+                for (int k = i+1; k<j; k++)
+                {
+                    sum += arr[k]*upTriangle[k][j];
+                }
+                upTriangle[i][j] = sum*upTriangle[i][i];
+            }
+        }
+            printMatrix(upTriangle);
+        return 1;
+    }
+    public static int reverse(double[][] arr)
+    {
+        double[] sigma = new double[arr.length];
+        double cos =0;
+        double sin = 0;
+        double temp = 0;
+        for(int k = arr.length-2; k >=0; k--)
+        {
+            for(int i = arr.length-1; i > k; i--)
+            {
+                sigma[i] = arr[i][k];
+                arr[i][k] = 0.;
+            }
+            for (int i = arr.length-1; i>k; i--)
+            {
+                if(Double.compare(sigma[i], 0) > 0)
+                {
+                    cos = sigma[i] - 1.;
+                    sin = Math.sqrt(1 - cos*cos);
+                } else
+                {
+                    cos = sigma[i] + 1.;
+                    sin = Math.sqrt(1 - cos*cos);
+                }
+                for(int l = 0; l < arr.length; l++)
+                {
+                    temp = arr[l][k];
+                    arr[l][k] = arr[l][k]*cos + arr[l][i]*sin;
+                    arr[l][i] = temp*(-sin) + arr[l][i]*cos;
+                }
+            }
+        }
+        return 1;
+    }
+}
---
+++ src/test/java/Testing.java
@@ -0,0 +1,104 @@
+import org.junit.Test;
+
+public class Testing
+{
+    final int size = 3;
+    final double[][] E = {{1,0,0}, {0,1,0},{0,0,1}};
+    /*
+0.5005689380030791 -0.4994310619969207 -1.3787600615856394E-4
+-0.4994310619969207 0.5005689380030793 -1.3787600615856394E-4
+-1.3787600615856394E-4 -1.3787600615856394E-4 0.001275752012317128
+    * */
+    @Test
+    public void TestModule() {
+        double[][] matrix = new double[size][];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = new double[size];
+        }
+        matrix[0][0] = 3;
+        matrix[0][1] = 2;
+        matrix[0][2] = 1 ;
+
+        matrix[1][0] = 2;
+        matrix[1][1] = 2;
+        matrix[1][2] = 1 ;
+
+        matrix[2][0] = 1;
+        matrix[2][1] = 1;
+        matrix[2][2] = 1;
+
+        double[][] matrixInverse = new double[size][];
+        for (int i = 0; i < matrix.length; i++) {
+            matrixInverse[i] = new double[size];
+        }
+
+        matrixInverse[0][0] = 1;
+        matrixInverse[0][1] = -1;
+        matrixInverse[0][2] = 0;
+
+        matrixInverse[1][0] = -1;
+        matrixInverse[1][1] = 2;
+        matrixInverse[1][2] = -1;
+
+        matrixInverse[2][0] = 0;
+        matrixInverse[2][1] = -1;
+        matrixInverse[2][2] = 2;
+
+       /*double[][] matrixInverse1 = new double[size][];
+        for (int i = 0; i < matrix.length; i++) {
+            matrixInverse1[i] = new double[size];
+        }
+
+        matrixInverse1[0][0] = 1;
+        matrixInverse1[0][1] = -1;
+        matrixInverse1[0][2] = 0.1;
+
+        matrixInverse1[1][0] = -1;
+        matrixInverse1[1][1] = 2;
+        matrixInverse1[1][2] = -1;
+
+        matrixInverse1[2][0] = 0;
+        matrixInverse1[2][1] = -1;
+        matrixInverse1[2][2] = 2; */
+
+        double[][] result = new double[matrix.length][matrixInverse[0].length];
+
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = MatrixService.matrixCell(matrix, matrixInverse, row, col);
+            }
+        }
+
+        double[][] R = result;
+        R = MatrixService.divMatrix(R, E);
+        double[][] Z = MatrixService.divMatrix(matrixInverse, matrixInverse);
+
+        double zNorm = MatrixService.matr_inf_norm(Z);
+        double reverseNorm = MatrixService.matr_inf_norm(matrixInverse);
+        double dzetta = zNorm/reverseNorm;
+
+        double norm = MatrixService.matr_inf_norm(matrix);
+
+        MatrixService.UpTriangleMatrix(matrixInverse);
+
+        //MatrixService.printMatrix(matrixInverse);
+
+
+        MatrixService.reverseMatrix(matrixInverse);
+
+        MatrixService.printMatrix(matrixInverse);
+
+        MatrixService.reverse(matrixInverse);
+
+        MatrixService.printMatrix(matrixInverse);
+
+
+        double[][] arr = MatrixService.divMatrix(matrix, matrixInverse);
+
+        System.out.println(MatrixService.matr_inf_norm(arr));
+        System.out.println(zNorm);
+        System.out.println(reverseNorm);
+        System.out.println(dzetta);
+
+    }
+}
